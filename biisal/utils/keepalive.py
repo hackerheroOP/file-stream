2c7891ec8f1736be9877c8756642a5f579a4ef16
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2024-03-14 03:40:01 +0530

    improve

--- biisal/utils/keepalive.py
+++ biisal/utils/keepalive.py
@@ -4,18 +4,17 @@ import aiohttp
 import traceback
 from biisal.vars import Var
 
-
 async def ping_server():
     sleep_time = Var.PING_INTERVAL
     while True:
-        await asyncio.sleep(sleep_time)
         try:
-            async with aiohttp.ClientSession(
-                timeout=aiohttp.ClientTimeout(total=10)
-            ) as session:
-                async with session.get(Var.URL) as resp:
-                    logging.info("Pinged server with response: {}".format(resp.status))
-        except TimeoutError:
-            logging.warning("Couldn't connect to the site URL..!")
-        except Exception:
+            async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=10)) as session:
+                async with session.get(Var.URL) as response:
+                    logging.info(f"Pinged server with response: {response.status}")
+        except asyncio.TimeoutError:
+            logging.warning("Couldn't connect to the server URL within the timeout limit..!")
+        except Exception as e:
+            logging.error(f"An unexpected error occurred: {str(e)}")
             traceback.print_exc()
+        finally:
+            await asyncio.sleep(sleep_time)
