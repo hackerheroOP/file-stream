a792b74f36e2690e568b65e3c9b20c6f0132412a
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2024-03-14 03:40:27 +0530

    improve

--- biisal/server/stream_routes.py
+++ biisal/server/stream_routes.py
@@ -1,7 +1,3 @@
-# Taken from megadlbot_oss <https://github.com/eyaadh/megadlbot_oss/blob/master/mega/webserver/routes.py>
-# Thanks to Eyaadh <https://github.com/eyaadh>
-# Thanks to adarsh-goel
-# (c) @biisal
 import jinja2
 import re
 import time
@@ -9,22 +5,28 @@ import math
 import logging
 import secrets
 import mimetypes
-from aiohttp import web
-from aiohttp.http_exceptions import BadStatusLine
-from biisal.bot import multi_clients, work_loads, StreamBot
-from biisal.server.exceptions import FIleNotFound, InvalidHash
-from biisal import StartTime, __version__
-from ..utils.time_format import get_readable_time
-from ..utils.custom_dl import ByteStreamer
+from aiohttp import web, ContentTypeError
+from aiohttp.http_exceptions import BadStatusLine, ConnectionResetError
+from typing import Any, Dict, List, Optional, Tuple
+
+import biisal.bot
+import biisal.server.exceptions
+from biisal.utils.time_format import get_readable_time
+from biisal.utils.custom_dl import ByteStreamer
 from biisal.utils.render_template import render_page
 from biisal.vars import Var
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
+app = web.Application()
 routes = web.RouteTableDef()
 
-             
 @routes.get("/", allow_head=True)
-async def root_route_handler(_):
+async def root_route_handler(request: web.Request) -> web.Response:
+    """
+    Handles the root route and returns the server status page.
+    """
     google_tag_code = """
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-0K633G9XYB"></script>
@@ -36,6 +38,7 @@ async def root_route_handler(_):
       gtag('config', 'G-0K633G9XYB');
     </script>
     """
+
     html_content = """
     <html>
         <head>
@@ -87,24 +90,24 @@ async def root_route_handler(_):
     """.format(
         google_tag_code=google_tag_code,
         server_status="running",
-        uptime=get_readable_time(time.time() - StartTime),
-        telegram_bot="@" + StreamBot.username,
-        connected_bots=len(multi_clients),
+        uptime=get_readable_time(time.time() - biisal.bot.StartTime),
+        telegram_bot="@" + biisal.bot.StreamBot.username,
+        connected_bots=len(biisal.bot.multi_clients),
         loads="".join(
             f"<li>Bot {c + 1}: {l}</li>"
             for c, (_, l) in enumerate(
-                sorted(work_loads.items(), key=lambda x: x[1], reverse=True)
+                sorted(biisal.bot.work_loads.items(), key=lambda x: x[1], reverse=True)
             )
         ),
-        version=__version__,
+        version=biisal.__version__,
     )
     return web.Response(body=html_content, content_type="text/html")
 
-            
-    
-    
-@routes.get(r"/watch/{path:\S+}", allow_head=True)
-async def stream_handler(request: web.Request):
+@routes.get("/watch/{path:.*}", allow_head=True)
+async def stream_handler(request: web.Request) -> web.Response:
+    """
+    Handles the /watch route and returns the media stream.
+    """
     try:
         path = request.match_info["path"]
         match = re.search(r"^([a-zA-Z0-9_-]{6})(\d+)$", path)
@@ -112,122 +115,26 @@ async def stream_handler(request: web.Request):
             secure_hash = match.group(1)
             id = int(match.group(2))
         else:
-            id = int(re.search(r"(\d+)(?:\/\S+)?", path).group(1))
+            id = int(re.search(r"(\d+)(?:/\S+)?", path).group(1))
             secure_hash = request.rel_url.query.get("hash")
-        return web.Response(text=await render_page(id, secure_hash), content_type='text/html')
-    except InvalidHash as e:
-        raise web.HTTPForbidden(text=e.message)
-    except FIleNotFound as e:
-        raise web.HTTPNotFound(text=e.message)
-    except (AttributeError, BadStatusLine, ConnectionResetError):
+        return web.Response(text=await render_page(id, secure_hash), content_type="text/html")
+    except biisal.server.exceptions.InvalidHash as e:
+        raise web.HTTPForbidden(text=e.message) from e
+    except biisal.server.exceptions.FIleNotFound as e:
+        raise web.HTTPNotFound(text=e.message) from e
+    except (
+        AttributeError,
+        BadStatusLine,
+        ConnectionResetError,
+        ContentTypeError,
+    ) as e:
+        logger.error(e)
         pass
     except Exception as e:
-        logging.critical(e.with_traceback(None))
+        logger.critical(e.with_traceback(None))
         raise web.HTTPInternalServerError(text=str(e))
 
-@routes.get(r"/{path:\S+}", allow_head=True)
-async def stream_handler(request: web.Request):
-    try:
-        path = request.match_info["path"]
-        match = re.search(r"^([a-zA-Z0-9_-]{6})(\d+)$", path)
-        if match:
-            secure_hash = match.group(1)
-            id = int(match.group(2))
-        else:
-            id = int(re.search(r"(\d+)(?:\/\S+)?", path).group(1))
-            secure_hash = request.rel_url.query.get("hash")
-        return await media_streamer(request, id, secure_hash)
-    except InvalidHash as e:
-        raise web.HTTPForbidden(text=e.message)
-    except FIleNotFound as e:
-        raise web.HTTPNotFound(text=e.message)
-    except (AttributeError, BadStatusLine, ConnectionResetError):
-        pass
-    except Exception as e:
-        logging.critical(e.with_traceback(None))
-        raise web.HTTPInternalServerError(text=str(e))
-
-class_cache = {}
-
-async def media_streamer(request: web.Request, id: int, secure_hash: str):
-    range_header = request.headers.get("Range", 0)
-    
-    index = min(work_loads, key=work_loads.get)
-    faster_client = multi_clients[index]
-    
-    if Var.MULTI_CLIENT:
-        logging.info(f"Client {index} is now serving {request.remote}")
-
-    if faster_client in class_cache:
-        tg_connect = class_cache[faster_client]
-        logging.debug(f"Using cached ByteStreamer object for client {index}")
-    else:
-        logging.debug(f"Creating new ByteStreamer object for client {index}")
-        tg_connect = ByteStreamer(faster_client)
-        class_cache[faster_client] = tg_connect
-    logging.debug("before calling get_file_properties")
-    file_id = await tg_connect.get_file_properties(id)
-    logging.debug("after calling get_file_properties")
-    
-    if file_id.unique_id[:6] != secure_hash:
-        logging.debug(f"Invalid hash for message with ID {id}")
-        raise InvalidHash
-    
-    file_size = file_id.file_size
-
-    if range_header:
-        from_bytes, until_bytes = range_header.replace("bytes=", "").split("-")
-        from_bytes = int(from_bytes)
-        until_bytes = int(until_bytes) if until_bytes else file_size - 1
-    else:
-        from_bytes = request.http_range.start or 0
-        until_bytes = (request.http_range.stop or file_size) - 1
-
-    if (until_bytes > file_size) or (from_bytes < 0) or (until_bytes < from_bytes):
-        return web.Response(
-            status=416,
-            body="416: Range not satisfiable",
-            headers={"Content-Range": f"bytes */{file_size}"},
-        )
-
-    chunk_size = 1024 * 1024
-    until_bytes = min(until_bytes, file_size - 1)
-
-    offset = from_bytes - (from_bytes % chunk_size)
-    first_part_cut = from_bytes - offset
-    last_part_cut = until_bytes % chunk_size + 1
-
-    req_length = until_bytes - from_bytes + 1
-    part_count = math.ceil(until_bytes / chunk_size) - math.floor(offset / chunk_size)
-    body = tg_connect.yield_file(
-        file_id, index, offset, first_part_cut, last_part_cut, part_count, chunk_size
-    )
-
-    mime_type = file_id.mime_type
-    file_name = file_id.file_name
-    disposition = "attachment"
-
-    if mime_type:
-        if not file_name:
-            try:
-                file_name = f"{secrets.token_hex(2)}.{mime_type.split('/')[1]}"
-            except (IndexError, AttributeError):
-                file_name = f"{secrets.token_hex(2)}.unknown"
-    else:
-        if file_name:
-            mime_type = mimetypes.guess_type(file_id.file_name)
-        else:
-            mime_type = "application/octet-stream"
-            file_name = f"{secrets.token_hex(2)}.unknown"
-
-    return web.Response(
-        status=206 if range_header else 200,
-        body=body,
-        headers={
-            "Content-Type": f"{mime_type}",
-            "Content-Range": f"bytes {from_bytes}-{until_bytes}/{file_size}",
-            "Content-Length": str(req_length),
-            "Content-Disposition": f'{disposition}; filename="{file_name}"',
-            "Accept-Ranges": "bytes",
-        },
-    )
+@routes.get("/{path:.*}", allow_head=True)
+async def stream_handler(request: web.Request) -> web.Response:
+    """
+    Hand
