6c30d3f64ea33a6d906e76f7b84fdd17d8e9e345
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2024-03-14 03:40:18 +0530

    improve

--- biisal/__main__.py
+++ biisal/__main__.py
@@ -1,5 +1,6 @@
-# (c) @biisal
-# (c) adars h-goel
+# (c) @biisal, Adarsh Goel
+# Initalizes and runs the StreamBot
+
 import os
 import sys
 import glob
@@ -7,19 +8,17 @@ import asyncio
 import logging
 import importlib
 from pathlib import Path
-from pyrogram import idle
-from .bot import StreamBot
+from pyrogram import Client as StreamBot
+from pyrogram.errors import LoginRequired
 from .vars import Var
-from aiohttp import web
 from .server import web_server
 from .utils.keepalive import ping_server
-from biisal.bot.clients import initialize_clients
 
 LOGO = """
- ____ ___ ___ ____    _    _     
-| __ )_ _|_ _/ ___|  / \  | |    
-|  _ \| | | |\___ \ / _ \ | |    
-| |_) | | | | ___) / ___ \| |___ 
+ ____ ___ ___ ____    _    _    
+| __ )_ _|_ _/ ___|  / \  | |   
+|  _ \| | | |\___ \ / _ \ | |   
+| |_) | | | | ___) / ___ \| |___
 |____/___|___|____/_/   \_\_____|"""
 
 logging.basicConfig(
@@ -30,47 +29,53 @@ logging.getLogger("aiohttp").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
-ppath = "biisal/bot/plugins/*.py"
-files = glob.glob(ppath)
-StreamBot.start()
-loop = asyncio.get_event_loop()
+async def import_plugins():
+    """Import all plugins from the plugins directory."""
+    plugins_dir = Path("biisal/bot/plugins")
+    plugins = [f.stem for f in plugins_dir.glob("*.py") if f.is_file()]
+    for plugin in plugins:
+        try:
+            importlib.import_module(f".plugins.{plugin}", package="biisal.bot")
+            print(f"Imported => {plugin}")
+        except Exception as e:
+            logging.error(f"Error importing plugin {plugin}: {e}")
 
+async def start_bot():
+    """Start the Telegram bot."""
+    try:
+        bot_info = await StreamBot.get_me()
+        StreamBot.username = bot_info.username
+    except LoginRequired:
+        logging.error("Failed to login to Telegram")
+        sys.exit(1)
 
 async def start_services():
+    """Start all services."""
     print('\n')
     print('------------------- Initalizing Telegram Bot -------------------')
-    bot_info = await StreamBot.get_me()
-    StreamBot.username = bot_info.username
+    await start_bot()
     print("------------------------------ DONE ------------------------------")
     print()
-    print(
-        "---------------------- Initializing Clients ----------------------"
-    )
+    print('---------------------- Initializing Clients ----------------------')
     await initialize_clients()
     print("------------------------------ DONE ------------------------------")
     print('\n')
     print('--------------------------- Importing ---------------------------')
-    for name in files:
-        with open(name) as a:
-            patt = Path(a.name)
-            plugin_name = patt.stem.replace(".py", "")
-            plugins_dir = Path(f"biisal/bot/plugins/{plugin_name}.py")
-            import_path = ".plugins.{}".format(plugin_name)
-            spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
-            load = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(load)
-            sys.modules["biisal.bot.plugins." + plugin_name] = load
-            print("Imported => " + plugin_name)
+    await import_plugins()
+    print('------------------------------ DONE ------------------------------')
+    print('\n')
     if Var.ON_HEROKU:
         print("------------------ Starting Keep Alive Service ------------------")
         print()
         asyncio.create_task(ping_server())
     print('-------------------- Initalizing Web Server -------------------------')
-    app = web.AppRunner(await web_server())
-    await app.setup()
+    app = await web_server()
+    await app.start()
     bind_address = "0.0.0.0" if Var.ON_HEROKU else Var.BIND_ADRESS
-    await web.TCPSite(app, bind_address, Var.PORT).start()
-    print('----------------------------- DONE ---------------------------------------------------------------------')
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, bind_address, Var.PORT)
+    print(f'-------------------- Server started at http://{bind_address}:{Var.PORT} --------------------')
     print('\n')
     print('---------------------------------------------------------------------------------------------------------')
     print('---------------------------------------------------------------------------------------------------------')
@@ -78,21 +83,22 @@ async def start_services():
     print('---------------------------------------------------------------------------------------------------------')
     print('\n')
     print('----------------------- Service Started -----------------------------------------------------------------')
-    print('                        bot =>> {}'.format((await StreamBot.get_me()).first_name))
+    print('                        bot =>> {}'.format(StreamBot.username))
     print('                        server ip =>> {}:{}'.format(bind_address, Var.PORT))
-    print('                        Owner =>> {}'.format((Var.OWNER_USERNAME)))
+    print('                        Owner =>> {}'.format(Var.OWNER_USERNAME))
     if Var.ON_HEROKU:
         print('                        app runnng on =>> {}'.format(Var.FQDN))
     print('---------------------------------------------------------------------------------------------------------')
     print(LOGO)
-    try: 
-        await StreamBot.send_message(chat_id=Var.OWNER_ID[0] ,text='<b>ᴊᴀɪ sʜʀᴇᴇ ᴋʀɪsʜɴᴀ 😎\nʙᴏᴛ ʀᴇsᴛᴀʀᴛᴇᴅ !!</b>')
-    except Exception as e:
-        print(f'got this err to send restart msg to owner : {e}')
+    if Var.OWNER_ID:
+        try:
+            await StreamBot.send_message(chat_id=Var.OWNER_ID[0], text='<b>ᴊᴀɪ sʜʀᴇᴇ ᴋʀɪsʜɴᴀ 😎\nʙᴏᴛ ʀᴇsᴛᴀʀᴛᴇᴅ !!</b>')
+        except Exception as e:
+            logging.error(f'got this err to send restart msg to owner : {e}')
     await idle()
 
 if __name__ == '__main__':
     try:
-        loop.run_until_complete(start_services())
+        asyncio.run(start_services())
     except KeyboardInterrupt:
         logging.info('----------------------- Service Stopped -----------------------')
