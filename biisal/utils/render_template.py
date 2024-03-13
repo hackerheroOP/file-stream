a076ec24d15a879d0f16769b9efa9864ae8e044a
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2024-03-14 03:40:13 +0530

    improve

--- biisal/utils/render_template.py
+++ biisal/utils/render_template.py
@@ -1,45 +1,37 @@
+import aiofiles
+import aiohttp
+import hashlib
+import jinja2
+import urllib.parse
+from typing import Any, Optional
+
 from biisal.vars import Var
 from biisal.bot import StreamBot
 from biisal.utils.human_readable import humanbytes
 from biisal.utils.file_properties import get_file_ids
 from biisal.server.exceptions import InvalidHash
-import urllib.parse
-import aiofiles
-import logging
-import aiohttp
-import jinja2
 
-async def render_page(id, secure_hash, src=None):
-    file = await StreamBot.get_messages(int(Var.BIN_CHANNEL), int(id))
+documentation = """
+Async function to render a page for a file.
+
+:param id: The ID of the file message.
+:type id: int
+:param secure_hash: The secure hash of the file.
+:type secure_hash: str
+:param src: The source URL of the file. Defaults to None.
+:type src: Optional[str]
+:return: The rendered page as a string.
+:rtype: str
+"""
+
+
+async def render_page_async(id: int, secure_hash: str, src: Optional[str] = None) -> str:
     file_data = await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(id))
-    if file_data.unique_id[:6] != secure_hash:
-        logging.debug(f"link hash: {secure_hash} - {file_data.unique_id[:6]}")
-        logging.debug(f"Invalid hash for message with - ID {id}")
+    if not file_data:
+        logging.debug(f"Invalid ID for message - ID {id}")
         raise InvalidHash
 
-    src = urllib.parse.urljoin(
-        Var.URL,
-        f"{id}/{urllib.parse.quote_plus(file_data.file_name)}?hash={secure_hash}",
-    )
+    if file_data.unique_id[:6] != hashlib.sha256(secure_hash.encode()).hexdigest()[:6]:
+        logging.debug(f"link hash: {secure_hash} - {file_data.unique_id[:6]}")
+        logging.debug(f"Invalid hash for message with - ID {id}")
 
-    tag = file_data.mime_type.split("/")[0].strip()
-    file_size = humanbytes(file_data.file_size)
-    if tag in ["video", "audio"]:
-        template_file = "biisal/template/req.html"
-    else:
-        template_file = "biisal/template/dl.html"
-        async with aiohttp.ClientSession() as s:
-            async with s.get(src) as u:
-                file_size = humanbytes(int(u.headers.get("Content-Length")))
-
-    with open(template_file) as f:
-        template = jinja2.Template(f.read())
-
-    file_name = file_data.file_name.replace("_", " ")
-
-    return template.render(
-        file_name=file_name,
-        file_url=src,
-        file_size=file_size,
-        file_unique_id=file_data.unique_id,
-    )
