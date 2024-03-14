ea89fe0a7474426a5bc5ef7f32cc5424d31fad2f
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2024-03-14 12:21:28 +0530

    comment

--- biisal/utils/file_size.py
+++ biisal/utils/file_size.py
@@ -1,7 +1,1 @@
-# This is a copyright notice, indicating that the code is owned by a specific entity or individual.
-# In this case, it appears that the code is owned by an entity holding the copyright (c) with ID 2.
-# It's important to include a copyright notice in any code that you write, to protect your intellectual property.
-
-# The next line is blank, which is a common convention in code formatting.
-# It can be used to separate different sections of code, making it easier to read and understand.
-
+# Copyright (c) 2
