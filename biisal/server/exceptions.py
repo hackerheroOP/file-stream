31f8d9d4bb693a7bcdfcf18a53fec13871dd7aa4
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2024-03-14 10:11:11 +0530

    comment

--- biisal/server/exceptions.py
+++ biisal/server/exceptions.py
@@ -1,7 +1,18 @@
 class InvalidHash(Exception):
+    """
+    This class represents an exception that is raised when an invalid hash is encountered.
+    The message attribute stores the error message associated with the exception.
+    """
     def __init__(self):
+        """
+        Initializes the InvalidHash exception with an error message.
+        """
         self.message = "Invalid hash"
 
 class FileNotFound(Exception):
+    """
+    This class represents an exception that is raised when a file is not found.
+    The message attribute stores the error message associated with the exception.
+    """
     def __init__(self):
-        self.message = "File not found"
+
