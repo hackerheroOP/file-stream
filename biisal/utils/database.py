bc5faee144915fb3c2b5ecc7eb7b962ef81e7d04
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2024-03-14 10:11:09 +0530

    comment

--- biisal/utils/database.py
+++ biisal/utils/database.py
@@ -4,9 +4,18 @@ import motor.motor_asyncio
 from typing import Optional
 
 class Database:
-    def __init__(self, uri: str, database_name: str):
-        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[database_name]
-        self.col = self.db.users
-        self.banned_list = self.db.banned_list
+    """
+    A class representing a database connection and its components.
+    """
 
+    def __init__(self, uri: str, database_name: str):
+        """
+        Initialize the Database object with a URI and database name.
+
+        :param uri: The URI for the database connection
+        :param database_name: The name of the database
+        """
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)  # Initialize the MongoDB client
+        self.db = self._client[database_name]  # Select the specified database
+        self.col = self.db.users  # Set a reference to the 'users' collection
+        self.banned_list = self.db.banned_list  # Set a reference to the 'banned_list' collection
