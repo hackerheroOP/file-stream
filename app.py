35b762179543a22c4d6b3111710396cba2307b87
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2024-03-14 09:44:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    """Return homepage."""
-    return render_template('home.html')
+def hello_world():
+    return 'Bisal_Files'
 
 @app.route("/favicon.ico")
 def favicon():
-    """Return a 204 No Content status code for favicon requests."""
     return "", 204
 
+
 if __name__ == "__main__":
-    """Run the Flask app in debug mode."""
     app.run(debug=True)
